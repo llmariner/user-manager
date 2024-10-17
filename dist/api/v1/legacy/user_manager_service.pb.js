572c7c29209edd6eaa34093d6eba11aec8598d9d
Guangrui Fu <guangrui@cloudnatix.com>
2024-10-17 23:20:27 +0000

    add internal api to create user

---
+++ dist/api/v1/legacy/user_manager_service.pb.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+import * as fm from "../../../fetch.pb";
+export class UsersInternalService {
+    static ListInternalAPIKeys(req, initReq) {
+        return fm.fetchReq(`/llmoperator.users.server.v1.UsersInternalService/ListInternalAPIKeys`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+    static ListInternalOrganizations(req, initReq) {
+        return fm.fetchReq(`/llmoperator.users.server.v1.UsersInternalService/ListInternalOrganizations`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+    static ListOrganizationUsers(req, initReq) {
+        return fm.fetchReq(`/llmoperator.users.server.v1.UsersInternalService/ListOrganizationUsers`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+    static ListProjects(req, initReq) {
+        return fm.fetchReq(`/llmoperator.users.server.v1.UsersInternalService/ListProjects`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+    static ListProjectUsers(req, initReq) {
+        return fm.fetchReq(`/llmoperator.users.server.v1.UsersInternalService/ListProjectUsers`, Object.assign(Object.assign({}, initReq), { method: "POST", body: JSON.stringify(req) }));
+    }
+}
